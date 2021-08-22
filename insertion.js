0fd81b24e7d0bdee9163704c54f69fbb598e6347
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-21 22:43:26 -0500

    passing insertion

--- insertion.js
+++ insertion.js
@@ -1,3 +1,16 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for(let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j;
+
+        for(j = i - 1; j >= 0 && arr[j] > current; j--) {
+            arr[j + 1] = arr[j]
+        }
+        arr[j + 1] = current;
+    }
+    return arr;
+}
 
 module.exports = insertionSort;
+
+[3,1,5,2,7,4]
